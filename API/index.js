5dd01fb250a8046001e2ae882fe4859ad1671147
hediipor <hediboumaiza2002@gmail.com>
2024-02-27 23:54:57 +0100

    secont commit

--- API/index.js
+++ API/index.js
@@ -26,7 +26,7 @@ const ListSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    cards: [CardSchema] // Embed Card documents in the cards array
+    cards: [CardSchema] 
 });
 
 const List = mongoose.model('List', ListSchema, 'List');
@@ -46,10 +46,42 @@ app.use(express.json());
 app.get("/getlists", async (request, response) => {
     try {
         const lists = await List.find().lean();
-        console.log('Lists from MongoDB:', lists);
         response.json(lists);
     } catch (error) {
         console.log("Error getting lists: ", error);
+        response.status(500).send("Server Error");
+    }
+});
+
+app.post("/addList", async (request, response) => {
+    try {
+        const { name } = request.body;
+        if (!name) {
+            return response.status(400).json({ error: 'Name is required for a new list' });
+        }
+        const newList = new List({ name });
+        await newList.save();
+        response.status(201).json(newList);
+    } catch (error) {
+        console.error("Error adding list: ", error);
+        response.status(500).send("Server Error");
+    }
+});
+
+app.delete("/deleteList/:id", async (request, response) => {
+    try {
+        const listId = request.params.id;
+        if (!listId) {
+            return response.status(400).json({ error: 'List ID is required for deletion' });
+        }
+        const existingList = await List.findById(listId);
+        if (!existingList) {
+            return response.status(404).json({ error: 'List not found' });
+        }
+        await existingList.remove();
+        response.status(200).json({ message: 'List deleted successfully' });
+    } catch (error) {
+        console.error("Error deleting list: ", error);
         response.status(500).send("Server Error");
     }
 });
